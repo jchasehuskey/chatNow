4a6c68f7563375e8346516557b4ecbd402e73741
jchasehuskey <101059291+jchasehuskey@users.noreply.github.com>
2023-03-18 12:33:12 -0600

    updated firebase

--- App.js
+++ App.js
@@ -4,13 +4,27 @@ import { StyleSheet, Text, View, TextInput, Button, Alert, TouchableOpacity } fr
 import Chat from './components/chat';
 import Start from './components/start';
 
-// import react Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Firebase and Firestore packages
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-// Create the navigator
-const Stack = createNativeStackNavigator();
+// initialize Firebase with your project's credentials
+const firebaseConfig = {
+  apiKey: "AIzaSyAVZ3KW3_LDCZ72VM74K9xieuo7fNR39qA",
+  authDomain: "chatnow-82382.firebaseapp.com",
+  projectId: "chatnow-82382",
+  storageBucket: "chatnow-82382.appspot.com",
+  messagingSenderId: "167868473092",
+  appId: "1:167868473092:web:c75acb2cb682e75d4eba51"
+};
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Create a reference to Firestore database
+const db = firebase.firestore();
 
 const App = () => {
   return (
@@ -21,10 +35,14 @@ const App = () => {
         <Stack.Screen
           name="Start"
           component={Start}
+          // pass the database object to the Start component
+          initialParams={{ db: db }}
         />
         <Stack.Screen
           name="Chat"
           component={Chat}
+          // pass the database object to the Chat component
+          initialParams={{ db: db }}
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -32,3 +50,43 @@ const App = () => {
 }
 
 export default App;
+
+
+
+
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { useState } from 'react';
+// import { StyleSheet, Text, View, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
+// import Chat from './components/chat';
+// import Start from './components/start';
+
+// // import react Navigation
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// // Create the navigator
+// const Stack = createNativeStackNavigator();
+
+
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         initialRouteName="Start"
+//       >
+//         <Stack.Screen
+//           name="Start"
+//           component={Start}
+//         />
+//         <Stack.Screen
+//           name="Chat"
+//           component={Chat}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
--- components/chat.js
+++ components/chat.js
@@ -7,6 +7,7 @@ import {
   KeyboardAvoidingView,
 } from "react-native";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { collection, orderBy, onSnapshot, addDoc } from "firebase/firestore";
 
 const renderBubble = (props) => {
   return (
@@ -28,24 +29,25 @@ export default function Chat(props) {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placebear.com/140/140",
-        },
-      },
-      {
-        _id: 2,
-        text: "You've entered the chat",
-        createdAt: new Date(),
-        system: true,
-      },
-    ]);
+    const messagesQuery = orderBy(collection(db, "messages"), "createdAt", "desc");
+    const unsubscribe = onSnapshot(messagesQuery, (querySnapshot) => {
+      const messages = querySnapshot.docs.map((doc) => {
+        const firebaseData = doc.data();
+
+        const data = {
+          _id: doc.id,
+          text: "",
+          createdAt: new Date(),
+          ...firebaseData,
+        };
+
+        return data;
+      });
+
+      setMessages(messages);
+    });
+
+    return unsubscribe;
   }, []);
 
   useEffect(() => {
@@ -64,10 +66,8 @@ export default function Chat(props) {
     });
   }, [props.route.params.name, props.route.params.color]);
 
-  const onSend = (newMessages = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
-    );
+  const onSend = (newMessages) => {
+    addDoc(collection(db, "messages"), newMessages[0]);
   };
 
   return (
@@ -78,7 +78,7 @@ export default function Chat(props) {
         messages={messages}
         renderBubble={renderBubble}
         onSend={(newMessages) => onSend(newMessages)}
-        user={{ _id: 1 }}
+        user={{ _id: props.route.params.userId, name: props.route.params.name }}
       />
       {Platform.OS === "android" ? (
         <KeyboardAvoidingView behavior="height" />
@@ -98,3 +98,105 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+
+// import React, { useState, useEffect } from "react";
+// import {
+//   View,
+//   Button,
+//   StyleSheet,
+//   Platform,
+//   KeyboardAvoidingView,
+// } from "react-native";
+// import { GiftedChat, Bubble } from "react-native-gifted-chat";
+
+// const renderBubble = (props) => {
+//   return (
+//     <Bubble
+//       {...props}
+//       wrapperStyle={{
+//         right: {
+//           backgroundColor: "#000",
+//         },
+//         left: {
+//           backgroundColor: "#FFF",
+//         },
+//       }}
+//     />
+//   );
+// };
+
+// export default function Chat(props) {
+//   const [messages, setMessages] = useState([]);
+
+//   useEffect(() => {
+//     setMessages([
+//       {
+//         _id: 1,
+//         text: "Hello developer",
+//         createdAt: new Date(),
+//         user: {
+//           _id: 2,
+//           name: "React Native",
+//           avatar: "https://placebear.com/140/140",
+//         },
+//       },
+//       {
+//         _id: 2,
+//         text: "You've entered the chat",
+//         createdAt: new Date(),
+//         system: true,
+//       },
+//     ]);
+//   }, []);
+
+//   useEffect(() => {
+//     // Retrieve the name and color values from the navigation prop
+//     let name = props.route.params.name;
+//     let color = props.route.params.color;
+
+//     // Set the header title to the name value
+//     props.navigation.setOptions({ title: name });
+
+//     // Set the header background color to the color value
+//     props.navigation.setOptions({
+//       headerStyle: {
+//         backgroundColor: color,
+//       },
+//     });
+//   }, [props.route.params.name, props.route.params.color]);
+
+//   const onSend = (newMessages = []) => {
+//     setMessages((previousMessages) =>
+//       GiftedChat.append(previousMessages, newMessages)
+//     );
+//   };
+
+//   return (
+//     <View
+//       style={[styles.container, { backgroundColor: props.route.params.color }]}
+//     >
+//       <GiftedChat
+//         messages={messages}
+//         renderBubble={renderBubble}
+//         onSend={(newMessages) => onSend(newMessages)}
+//         user={{ _id: 1 }}
+//       />
+//       {Platform.OS === "android" ? (
+//         <KeyboardAvoidingView behavior="height" />
+//       ) : null}
+//       {/* optional for IOS */}
+//       {Platform.OS === "ios"?<KeyboardAvoidingView behavior="padding" />: null}
+//       <Button
+//         title="Leave Chat"
+//         onPress={() => props.navigation.navigate("Start")}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
